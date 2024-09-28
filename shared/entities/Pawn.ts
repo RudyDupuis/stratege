9fca30ba48b2bf02673cadc64ed4325869f63009
Rudy <abcdupuisrudy@gmail.com>
2024-09-28 16:50:16 +0200

    Ajout de prettier et de Eslint dans shared

--- shared/entities/Pawn.ts
+++ shared/entities/Pawn.ts
@@ -1,13 +1,13 @@
 export class Pawn {
-  public readonly owner: string;
-  public remainingMode: number;
-  public orientation: "N" | "S" | "E" | "W";
-  public isAlive: boolean;
+  public readonly owner: string
+  public remainingMode: number
+  public orientation: 'N' | 'S' | 'E' | 'W'
+  public isAlive: boolean
 
-  constructor(owner: string, orientation: "N" | "S" | "E" | "W") {
-    this.owner = owner;
-    this.remainingMode = 2;
-    this.orientation = orientation;
-    this.isAlive = true;
+  constructor(owner: string, orientation: 'N' | 'S' | 'E' | 'W') {
+    this.owner = owner
+    this.remainingMode = 2
+    this.orientation = orientation
+    this.isAlive = true
   }
 }
